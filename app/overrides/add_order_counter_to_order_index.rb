6bdefe4e71cb5a02b9ae2bbb14bd5d412ae6a8af
Roel van Hoof <r.v.hoof@gmail.com>
2016-11-08 09:31:45 +0100

    move order counter to third column

--- app/overrides/add_order_counter_to_order_index.rb
+++ app/overrides/add_order_counter_to_order_index.rb
@@ -2,7 +2,7 @@
 Deface::Override.new(
     :virtual_path  => "spree/admin/orders/_filter",
     :name => "add_order_counter_to_order_index",
-    :insert_bottom => "[data-hook='admin_orders_index_search_filters_fourth_column']",
+    :insert_bottom => "[data-hook='admin_orders_index_search_filters_third_column']",
     :partial => "spree/admin/orders/add_order_counter_to_order_index",
     :disabled => false,
 )
--- spree_admin_order_counter.gemspec
+++ spree_admin_order_counter.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_admin_order_counter'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.summary     = 'Add order counter to spree admin'
   s.description = 'Add order counter to spree admin'
   s.required_ruby_version = '>= 1.9.3'
